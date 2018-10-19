00d319a6acdde9402c1e78057a28a9761575fa87
zhijianzou <justinchow10@hotmail.com>
2018-10-19 14:02:24 +0200

    changed the order in cocktail model

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,7 +1,7 @@
 class Cocktail < ApplicationRecord
+  mount_uploader :photo, PhotoUploader
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
   validates :name, presence: true
   validates :name, uniqueness: true
-  mount_uploader :photo, PhotoUploader
 end
